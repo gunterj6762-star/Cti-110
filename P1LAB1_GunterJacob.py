044a5388bdb512a6b420d649c3a9781e64194aa3
gunterj6762-star <gunterj6762@student.faytechcc.edu>
2025-09-28 22:35:00 -0400

    Add files via upload
    simple python code where you input your name and it responds back with your name

---
+++ P1LAB1_GunterJacob.py
@@ -0,0 +1,10 @@
+# Jacob Gunter
+# 09/28/2025
+# P1LAB1
+# This program asks the user for their first and last name
+# and then displays a welcome message.
+
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Welcome to CTI-110,", first_name, last_name + "!")
